85c3fbf75c68da9ec551de0346090bf79b52236c
t-thomas-dev <134882797+t-thomas-dev@users.noreply.github.com>
2023-11-30 20:55:55 -0700

    login

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("✅ Login success");
+        location.reload();
+    } else {
+        alert("🛑 Incorrect username or password");
+    }
+})
