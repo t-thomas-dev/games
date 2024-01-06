083a3361f34ebdc2f1a48f669562489773d80016
t-thomas-dev <134882797+t-thomas-dev@users.noreply.github.com>
2024-01-06 14:19:00 -0700

    add tooltips and login changes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-
-    if (username === "user" && password === "webdev") {
-        document.cookie="SessionLoginSuccess-1";
-        alert("✅ Login success");
-        window.location.replace("https://t-thomas-dev.github.io/games/");
-    } else {
-        alert("🛑 Incorrect username or password");
-    }
-})
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 /******algolia search js******/
 
